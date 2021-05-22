0a4593e16ca26aec72508361db740bcfb21c1557
Diego Varela <diegocrash124@gmail.com>
2021-05-21 23:43:38 -0600

    Se crea la base del gui

---
+++ Avance_de_proyecto/src/avance_de_proyecto/Avance_de_proyecto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avance_de_proyecto;
+
+/**
+ *
+ * @author diego
+ */
+public class Avance_de_proyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
